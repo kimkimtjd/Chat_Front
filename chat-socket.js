85e834fd6fc9edcd97be1f679a74391d57826111
UglystoneDeveloper <kimeende@naver.com>
2023-04-11 10:14:23 +0900

    te

--- chat-socket.js
+++ chat-socket.js
@@ -1,20 +1,20 @@
 const socket = io("ws://118.67.135.60:3000/")
 // const socket = io("ws://localhost:3000/")
-const nickname = prompt('닉네임을 알려주세요.');
-const room = prompt('입장할 방의 코드를 적어주세요.');
+// var nickname = "운영자";
+// const room = "d67dc57d-14a3-488b-8f5f-dfeee417ed3c"
 
 const message = document.getElementById('message');
 const messages = document.getElementById('messages');
 const messagesub = document.getElementById('messagesub');
 
-const handleSubmitNewMessage = () => {
-  socket.emit('message', nickname + ":" + message.value +"방이름" + room )
-
+function Test(arg , chat , roomname){
+ socket.emit('message', arg + ":" + chat +"방이름" + roomname)
+ socket.emit('room', roomname)             
+ nickname = arg
 }
 
 socket.on('message', (data) => {
   handleNewMessage(data);
-
 })
 
 const handleNewMessage = (message) => {
@@ -27,19 +27,44 @@ const handleNewMessage = (message) => {
   // }
 }
 
+
 const buildNewMessage = (message) => {
-  if(message.split(":")[0] === nickname ){
-  const li = document.createElement("li");
-  li.appendChild(document.createTextNode(message.split("방이름")[0]))
-  return li;
+  console.log(nickname)
+  if(message.split(":")[0] === nickname){
+  
+    const div = document.createElement("div");
+    div.classList.add('senderbox');
+    div.appendChild(sendMessage(message));
+
+  return div;
   }
   else{
-    const li = document.createElement("li");
-    li.classList.add('test');
+
+    const div = document.createElement("div");
     let text = document.createTextNode(message.split("방이름")[0]);
-    li.appendChild(text);
-    document.body.appendChild(li)
-    return li;
-  
+    div.classList.add('receiverbox');
+
+    div.appendChild(text);
+    document.body.appendChild(div)
+    return div;
+ 
   }
 }
+
+const sendMessage = (message) => {
+  const span = document.createElement("span");
+  span.classList.add('sender');
+  
+  span.appendChild(document.createTextNode(message.split("방이름")[0]))
+
+  return span
+}
+
+/****************************** 아래 코드는 웹용  위 부분은 공용******************************/ 
+// socket.emit('room', room)             
+             
+// const handleSubmitNewMessage = () => {
+//   socket.emit('message', nickname + ":" + message.value +"방이름" + room )
+// }
+
+
