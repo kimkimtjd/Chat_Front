fc689636b6227be5b0a178ab24287bc0fdb72ab2
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-04-28 10:46:43 +0900

    Update chat-socket.js

--- chat-socket.js
+++ chat-socket.js
@@ -42,7 +42,7 @@ function Room(roomname, pk , user , partner , logo_image) {
   nickname = "애완용꿀꿀이"//user
 
   // 아래 2개는 삭제 , 상단 애완용 꿀꿀이는 user 로 변경예정
-  user = ""
+  user = "애완용꿀꿀이"
   pk = 1
   partner = "lee"
 
