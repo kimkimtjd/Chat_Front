b53cb80a997be63692a25d127f4c1a32f80e7934
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-04-28 14:18:19 +0900

    Update chat-socket.js

--- chat-socket.js
+++ chat-socket.js
@@ -339,11 +339,11 @@ const receivesecondMessage = (datesecond) => {
 
 
 /****************************** 아래 코드는 웹용  위 부분은 공용******************************/
-socket.emit('room', room)
+// socket.emit('room', room)
 
-// window.onload = function() {
-//      Room()   
-//  };
+window.onload = function() {
+    window.scrollBy(0, window.innerHeight);
+ };
 
 // const handleSubmitNewMessage = () => {
 //   // socket.emit('message', nickname + ":" + message.value + "방이름" + room)
