cdcc98710a1e2a581d5fca4c1f2783455a3b26cc
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-05-18 15:26:22 +0900

    Update chat-socket.js

--- chat-socket.js
+++ chat-socket.js
@@ -405,13 +405,13 @@ const sendMessage = (message) => {
       div_info_first_img_img.classList.add("businesscard_info_first_logo_img")
 
       div_info_total.prepend(div_info_first)
-      div_info_total.appendChild(div_info_first_box)
       div_info_first.prepend(div_info_first_img)
       div_info_first_img.prepend(div_info_first_img_img)
 
       
       const div_info_first_box = document.createElement("div");
       div_info_first_box.classList.add("businesscard_info_first_list")
+      div_info_total.appendChild(div_info_first_box)
 
       /* 닉네임 */
       const div_info_first_nickname_box = document.createElement("div");
