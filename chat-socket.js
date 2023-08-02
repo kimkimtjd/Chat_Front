8eeb2f154ff612ab5145389aad267e081aeaa76e
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-08-02 17:14:35 +0900

    Update chat-socket.js

--- chat-socket.js
+++ chat-socket.js
@@ -111,10 +111,12 @@ function Room(roomname, pk, user, partner, logo_image) {
       }
 
       /* 스크롤 하단으로 이동 */
-      setTimeout(() => {
+    if(currentPage === 1){
+        setTimeout(() => {
           const messagestest = document.getElementById('messages');
           messagestest.scrollTop = messagestest.scrollHeight;
         }, 1000);  
+      }
       
       
     })
