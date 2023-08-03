79e194375b7d6f9f54cdb9c8a6f6d812995446ff
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-08-03 10:32:15 +0900

    Update chat-socket.js

--- chat-socket.js
+++ chat-socket.js
@@ -197,8 +197,8 @@ messagestest.addEventListener('scroll', function() {
     var scrollTop = messagestest.scrollTop;
     var clientHeight = messagestest.clientHeight;
 var scrollTop = window.scrollY;
-    if (scrollTop === 0 && !loading) {
-        loading = true;
+    if (scrollTop === 0) {
+        // loading = true;
         currentPage++; // 다음 페이지로 변경
         Room();
     
