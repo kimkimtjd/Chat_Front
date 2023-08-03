cb44cd6f3a6da78dbc79a4c1078c0b85daffa2c8
SwonWon Kim_Practice <88889079+kimkimtjd@users.noreply.github.com>
2023-08-03 10:20:26 +0900

    Update chat-socket.js

--- chat-socket.js
+++ chat-socket.js
@@ -62,16 +62,17 @@ var logo_image_second = ""
 /* 방입장 */
 function Room(roomname, pk, user, partner, logo_image) {
 
-  nickname = user
-  partner_user = partner
+  // nickname = user
+  // partner_user = partner
+
   /* 아래는 웹 테스트용 inputbox */
-    // nickname = "애완용꿀꿀이"
-    // user = nickname
-    // pk = 1
-    // partner = "kmskms"
-    // roomname = "b1b32f2f-9ab6-4570-b824-e700c55c1ba8"
-    // logo_image = ""
-    // partner_user = partner
+    nickname = "애완용꿀꿀이"
+    user = nickname
+    pk = 1
+    partner = "우리자원"
+    roomname = "724bde73-295a-4e10-8bf7-467f81c4d3dd"
+    logo_image = ""
+    partner_user = partner
   
   socket.emit('room', roomname)
 
